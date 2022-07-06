28096cc0cea80d37e863c5915cea49a2963b628e
Ahmed El-Deeb <ahmed.h.eldeep@gmail.com>
2022-07-06 23:53:38 +0200

    Add project files.

---
+++ Models/HRDatabaseContext.cs
@@ -0,0 +1,15 @@
+﻿using Microsoft.EntityFrameworkCore;
+
+namespace EmployeesApp.Models
+{
+    public class HRDatabaseContext : DbContext
+    {
+        public DbSet<Department> Departments { get; set; }
+        public DbSet<Employee> Employees { get; set; }
+
+        protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+        {
+            optionsBuilder.UseSqlServer(@"data source=DESKTOP-0K11BTV; initial catalog=EmployeeDB; integrated security=SSPI;");
+        }
+    }
+}
